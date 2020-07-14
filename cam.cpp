d34a6b5878515af70118bbeac6c1a1fc27fc95d2
Manuel Garcia Diaz Santos <manujugon@gmail.com>
2020-07-14 13:07:12 +0200

    Desarrollo de dataflow
    - Subido codigo en desarrollo

--- cam.cpp
+++ cam.cpp
@@ -2,46 +2,104 @@
 #include "ap_int.h"
 #include "cam.h"
 
-//#define NODE_MASK 0xF // ((1<<4)-1) Cada nodo ocupa 4 bits. Esto es una máscara que he creado para
-//#define RELATION_MASK 0x3 // ((1<<2)-1 )Expresarlo en hexadecimal.
-//#define TREE_SIZE 1024
-#define SEARCH_MODE 0
 
-// Declaracion de arbol estático
- void busqueda_cam (edge_t tree[TREE_SIZE], node_t nodo, rel_t relationship, bool order, hls::stream<node_t> *result) {
-	/*unsigned  position_hi, position_lo = 0;
-	if (order) {  //Significa que buscamos nuestros hijos
-		position_lo = ;
-		position_hi = WORD_BITS - 1;
-	} else {
-		position_lo = RELATION_BITS;
-		position_hi = (RELATION_BITS+NODE_BITS)-1;
-	}  // Significa que buscamos nuestros padres */
+
+
+void busqueda_cam (edge_t tree [TREE_SIZE], node_t nodo, rel_t relationship, bool fatherSearch, hls::stream<node_t> &result) {
+
+//He decidido conservar la directiva para indicarle que use una BRAM de dos puertos, pero sigue sin usarlas.
+#pragma HLS RESOURCE variable=tree  core=RAM_2P_BRAM
+
+// Aqui creo los canales de entrada para combinar().
+	hls::stream<node_t> in1;
+	hls::stream<node_t> in2;
+
+// Empiezo a utilizar el combinar(), pero no tengo claro si iría aquí o en el for.
+	combinar(in1,in2,result);
+
 #ifndef __SYNTHESIS__
-
 	int accesos = 0;
 #endif
-	ap_uint<EDGE_BITS> valor = 0;
+	ap_uint<EDGE_BITS> valor_min, valor_max = 0;
 	ap_uint<REL_BITS> node_relation =0;
-	ap_uint<NODE_BITS> compare_node=0;
-	for (int i = 0; i < TREE_SIZE; i++) {
-#ifndef __SYNTHESIS__
-	accesos++;
-#endif
-			  valor = tree[i];
+	ap_uint<NODE_BITS> compare_node_min, compare_node_max=0;
+	bool flag_min, flag_max= false;
 
-				  node_relation = valor(1,0);
-				  if (order) compare_node = SRC_NODE(valor);
-				  else compare_node = DST_NODE(valor);
-				  if ((compare_node == nodo) && (relationship == node_relation))  result->write_nb(compare_node);
-			  	 //result->write_nb(
-
+	// Esto es algo que he incorporado para controlar casos absurdos como encontrar el padre del nodo raíz. Nos ahorra problemas
+	//y estar haciendo lecturas innecesarias si se produce el caso. He utilizado no bloqueantes para perder el menor tiempo posible.
+	// ¿Como lo ves?
+	if (nodo == 1 && fatherSearch) {
+		in1.write_nb(EOT);
+		in2.write_nb(EOT);
+		return;
 	}
-#ifndef __SYNTHESIS__
-	std::cout << "El numero de lecturas del arbol han sido " << accesos << std::endl;
-#endif
+
+	/*Aqui es donde tengo muchas dudas. Entiendo que el DATAFLOW es para realizar tareas concurrentemente pero por ejemplo, como
+	 * lo usaria aqui? Tendria que dividir el for en dos FOR distintos, uno qu vaya de 0 a N/2 -1 y otro que vaya de N/2 a N y
+	 * utilizar el pramga DATAFLOW en cada uno de ellos? O estoy pasando algo por alto y no estoy viendo bien como utilizar
+	 * el pragma?
+	 */
+	for (int i = 0, j=TREE_SIZE-1; i < j; i++,j--) {
+	#pragma HLS DATAFLOW
+		#ifndef __SYNTHESIS__
+		accesos++;
+		#endif
+		valor_min = tree[i];
+		valor_max = tree[j];
+		node_relation = valor_min(1,0);
+		if (fatherSearch){
+			compare_node_min = DST_NODE(valor_min);
+			compare_node_max = DST_NODE(valor_max);
+		} else{
+			compare_node_min = SRC_NODE(valor_min);
+			compare_node_max = SRC_NODE(valor_max);
+		}
+
+		// IF para encontrar padres/nodos del for de N/2
+		if ((compare_node_min == nodo) && (relationship == node_relation) && fatherSearch) {
+			in1.write(SRC_NODE(valor_min));
+			break;
+				  }
+
+		if ((compare_node_min == nodo) && (relationship == node_relation) && !fatherSearch) {
+			in1.write(DST_NODE(valor_min));
+			flag_min = true;
+		} else if (flag_min == true) break;
+		// IF para encontrar padres/nodos del for de N/2
+		if ((compare_node_max == nodo) && (relationship == node_relation) && fatherSearch) {
+			in2.write(SRC_NODE(valor_max));
+			break;
+		}
+		if ((compare_node_max==nodo) && (relationship == node_relation) && !fatherSearch)  {
+			in2.write(DST_NODE(valor_max));
+			flag_max=true;
+		} else if (flag_max == true) break;
+	}
+	#ifndef __SYNTHESIS__
+		std::cout << "El numero de lecturas del arbol han sido " << accesos << std::endl;
+	#endif
+	return;
 }
 
- //BUSCAR SOFTWARE DE GENRACION DE ARBOLES
- // REPRESNTACION GRAFICA DEL ARBOL
- // SCRIPT PARA INCLUIRLO EN EL CPP
+/* Aqui te dejo el metodo del MUX, el combinar. Creo que la lógica está bien, faltaria probarlo pero hasta que no
+ * me quede claro lo de DATAFLOW no creo que pueda avanzar.
+ */
+void combinar(hls::stream<node_t> &in1, hls::stream<node_t> &in2, hls::stream<node_t> &out) {
+	bool end_1, end_2 = false;
+	bool isData_1, isData_2 = false;
+	node_t val1,val2;
+	do {
+		isData_1 = in1.read_nb(val1);
+		isData_2 = in2.read_nb(val2);
+	if (isData_1) {
+		if (val1 == EOT) end_1=true;
+		else out.write(val1);
+	}
+	if (isData_2) {
+		if (val2 == EOT)end_2=true;
+	}	else out.write(val2);
+
+	} while ((!end_1) || (!end_2));
+}
+
+
--- cam.h
+++ cam.h
@@ -1,109 +1,112 @@
 
 //Incluir cabecera parae evitar recursividades.
+#ifndef CAM_H_
+#define CAM_H_
+	#include "hls_stream.h"
+	#include "ap_int.h"
 
-#include "hls_stream.h"
-#include "ap_int.h"
+	#ifndef SEARCH_MODE
+	#define SEARCH_MODE 1
+	#endif
 
-#ifndef SEARCH_MODE
-#define  SEARCH_MODE 1 // Si no definimos la busqueda, por defecto será BFS
+	#ifndef TREE_SIZE
+	#define TREE_SIZE 128
+	#endif
+
+
+	#if TREE_SIZE == 128
+		#define NODE_BITS 8
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_128.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_128.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 256
+		#define NODE_BITS 9
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_256.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_256.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 512
+		#define NODE_BITS 10
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_512.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_512.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 1024
+		#define NODE_BITS 11
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_1024.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_1024.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 2048
+		#define NODE_BITS 12
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_2048.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_2048.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 4096
+		#define NODE_BITS 13
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_4096.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_4096.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 8192
+		#define NODE_BITS 14
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_8192.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_8192.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 16384
+		#define NODE_BITS 15
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_16384.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_16384.txt"
+			#endif
+	#endif
+
+	#if TREE_SIZE == 32768
+		#define NODE_BITS 16
+			#if SEARCH_MODE == 1
+				#define TREE_FILE "hls_tree_bfs_32768.txt"
+			#else
+				#define TREE_FILE "hls_tree_dfs_32768.txt"
+			#endif
+	#endif
+
+	#define REL_BITS 2
+	#define EDGE_BITS (NODE_BITS*2 + REL_BITS)
+	#define SRC_NODE(e) ((e)(EDGE_BITS-1,EDGE_BITS-NODE_BITS))
+	#define DST_NODE(e) ((e)(EDGE_BITS-NODE_BITS-1,REL_BITS))
+	#define EOT TREE_SIZE+1
+
+	typedef ap_uint<EDGE_BITS> edge_t;
+	typedef ap_uint<NODE_BITS> node_t;
+	typedef ap_uint<REL_BITS> rel_t;
+
+	void busqueda_cam (edge_t tree[TREE_SIZE], node_t nodo, rel_t relationship, bool fatherSearch, hls::stream<node_t> &result);
+	void combinar(hls::stream<node_t> &in1, hls::stream<node_t> &in2, hls::stream<node_t> &out);
+
 #endif
-
-#ifndef TREE_SIZE
-#define TREE_SIZE 128
-#endif
-
-#if TREE_SIZE == 128
-	#define NODE_BITS 8
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_tree_bfs_128.txt"
-		#else
-			#define TREE_FILE "hls_tree_dfs_128.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 256
-	#define NODE_BITS 9
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_256.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_256.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 512
-	#define NODE_BITS 10
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_512.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_512.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 1024
-	#define NODE_BITS 11
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_1024.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_1024.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 2048
-	#define NODE_BITS 12
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_2048.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_2048.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 4096
-	#define NODE_BITS 13
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_4096.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_4096.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 8192
-	#define NODE_BITS 14
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_8192.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_8192.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 16384
-	#define NODE_BITS 15
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_16384.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_16384.txt"
-		#endif
-#endif
-
-#if TREE_SIZE == 32768
-	#define NODE_BITS 16
-		#if SEARCH_MODE == 1
-			#define TREE_FILE "hls_bfs_tree_32768.txt"
-		#else
-			#define TREE_FILE "hls_dfs_tree_32768.txt"
-		#endif
-#endif
-
-
-#define REL_BITS 2
-#define EDGE_BITS (NODE_BITS*2 + REL_BITS)
-#define SRC_NODE(e) ((e)(EDGE_BITS-1,EDGE_BITS-NODE_BITS))
-#define DST_NODE(e) ((e)(EDGE_BITS-NODE_BITS-1,REL_BITS))
-
-typedef ap_uint<EDGE_BITS> edge_t;
-typedef ap_uint<NODE_BITS> node_t;
-typedef ap_uint<REL_BITS> rel_t;
-
-void busqueda_cam (edge_t tree[TREE_SIZE], node_t nodo, rel_t relationship, bool order, hls::stream<node_t> *result);
-
-
 
