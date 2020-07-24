17e8e56ab7227f6c9e3108d7de5c98f5debf54d8
Manuel Garcia Diaz Santos <manujugon@gmail.com>
2020-07-24 11:01:33 +0200

    Actualizacion generacion del arbol
    - Se ha subido el cpp de la generacion del arbol y su .h.
    - Se han realizado correctamente la generacion de los hijos en funcion de si hemos recorrido el arbol de manera DFS o BFS.
    - Se ha generado adicionalmente los grafos necesarios para distinguir BFS y DFS en graphviz.

--- tree.h
+++ tree.h
@@ -2,17 +2,22 @@
 
 struct Nodo {
 	unsigned int id_nodo;
-	unsigned int id_padre;
+	Nodo* padre;
 	unsigned int n_hijos;
 	unsigned short relacion;
-	std::vector<Nodo> nodos_hijos;
+	std::vector<Nodo*> nodos_hijos;
+
+	Nodo() : id_nodo(0), padre(nullptr),n_hijos(0),relacion(0) {}
 };
 
 
-void initNode(struct Nodo* n, unsigned int id, unsigned int nChilds, unsigned short rel, unsigned int padre);
-void initNode(struct Nodo* n, unsigned int id, unsigned int nChilds, unsigned short rel);
-void tree_generator(int nodos, int max_hijos);
-void graphGenerator(std::vector<Nodo> arbol);
-void treeToHLS(std::vector<Nodo> root, unsigned int necessaryBits);
+void initNode(struct Nodo& nodo, unsigned int id, unsigned int n_hijos, unsigned short rel, struct Nodo* padre);
+void treeGenerator(int nodos, int max_hijos);
+void reorderTreeBFS(std::vector<Nodo*> arbol);
+void reorderTreeDFS(std::vector<Nodo*> arbol);
+void treeData(std::vector<Nodo*> arbol);
+void graphGenerator(std::vector<Nodo*> arbol, bool isDfs);
+void bfstreeToHLS(std::vector<Nodo*> arbol, unsigned int necessaryBits);
+void dfstreeToHLS(std::vector<Nodo*> arbol, unsigned int neccessaryBits);
 unsigned int bits_needed(uint32_t value);
 std::string toBinary(int n);
--- tree_generator.cpp
+++ tree_generator.cpp
@@ -4,87 +4,231 @@
 #include <queue>
 #include <list>
 #include <fstream>
+#include <string>
 #include "tree.h"
+#include <stack>
+#include <math.h>
 
-
-void initNode (struct Nodo *n, int id, int nChilds, short rel) {
-	n->id_nodo = id;
-	n->n_hijos = nChilds;
-	n->relacion = rel;
-
+static int tree_size;
+bool idSorter(struct Nodo *a, struct Nodo *b) {
+	return (a->id_nodo < b->id_nodo);
+}
+void initNode(struct Nodo &nodo, unsigned int id, unsigned int n_hijos, unsigned short rel, struct Nodo* padre) {
+	nodo.id_nodo = id;
+	nodo.n_hijos = n_hijos;
+	nodo.relacion = rel;
+	nodo.padre = padre;
 }
 
-void tree_generator(int nodos, int max_hijos) {
-	  srand((unsigned) time(0));
-	  int randomNumber;
 
-
-	 /* for (int index = 0; index < 20; index++) {
-	    randomNumber = (rand() % (max_hijos+1));
-	    std::cout << randomNumber << std::endl;
-	  } */
-	int contador = 1;
-	int contador_nodos = 2;
-	Nodo actual,hijo, temp,raiz;
-	std::queue<int> hijos_libres;
-	std::queue<Nodo> nodos_pendientes;
-	std::vector<Nodo> tree;
-	initNode(&raiz,1,(rand() % ((max_hijos) + 1)),(rand()% 4));
-	nodos_pendientes.push(raiz);
-	int position=0;
-	while (tree.size() < nodos){
-		if (nodos_pendientes.empty()) {
-
-			/* Si no hemos llegado al numero de nodos que nos pide el usuario, lo que hacemos es coger alguna posicion
-			de la lista de nodos que pueden aumentar sus hijos. Lo cogemos y le aumentamos en 1 el numero de hijos que
-			dicho nodo puede tener, le hacemos los cambios pertinentes y lo sustituimos en el arbol debido a que hemos modificado
-			alguno de sus datos */
-
-			position=hijos_libres.front();
+void treeGenerator(int nodos, int max_hijos) {
+	tree_size = nodos;
+	int counter = 2;
+	std::vector<Nodo*> arbol;
+	std::queue<Nodo*> analizar;
+	std::queue<Nodo*> hijos_libres;
+	Nodo* hijo;
+	Nodo* actual;
+	Nodo* temp;
+	Nodo raiz = Nodo();
+	initNode(raiz, 1, (rand() % max_hijos)+1,0,NULL);
+	analizar.push(&raiz);
+	while (arbol.size() < nodos ) {
+		if (analizar.empty()) {
+			temp = hijos_libres.front();
 			hijos_libres.pop();
-			temp = tree[position];
-			temp.n_hijos +=1;
-			initNode(&hijo,contador_nodos,(rand()%(max_hijos+1)),(rand()%4));
-			temp.hijos.push_back(hijo.id_nodo);
-			if (temp.n_hijos < max_hijos) hijos_libres.push(position);
-			nodos_pendientes.push(hijo);
-			tree[position]=temp;
-			contador_nodos++;
+			temp->n_hijos += 1;
+			hijo = new Nodo;
+			initNode(*hijo, counter, rand() % max_hijos, 0, temp);
+			temp->nodos_hijos.push_back(hijo);
+			counter++;
+			analizar.push(hijo);
+			if ((hijo->n_hijos) < max_hijos) hijos_libres.push(hijo);
 		}
+		actual = analizar.front();
+		analizar.pop();
+		if (counter < nodos) {
+			for (int i = 0; i < actual->n_hijos; i++) {
+				if (counter < nodos) {
+					hijo = new Nodo;
+					initNode(*hijo, counter, rand() % max_hijos, 0, actual);
+					actual->nodos_hijos.push_back(hijo);
+					analizar.push(hijo);
+					counter++;
+					if (hijo->n_hijos < max_hijos) hijos_libres.push(hijo);
 
-		/* Cogemos un nodo de los pendientes de procesar y efectuamos esta operacion:
-		 * Crear sus hijos (de manera aleatoria), rellenar sus datos e introducirlo en el árbol y en el marcador de hijos libres
-		 * si es que es candidato a expandir el numero de hijos posteriormente
-		 */
+				}
 
-		actual = nodos_pendientes.front();
-		nodos_pendientes.pop();
-
-		if (contador_nodos < nodos) {
-			for (int k=0;k<actual.n_hijos;k++){
-				initNode(&hijo,contador_nodos,(rand()%(max_hijos+1)),(rand()%4));
-				actual.hijos.push_back(hijo.id_nodo);
-				nodos_pendientes.push(hijo);
-				contador_nodos++;
 			}
-		} ;
-		tree.push_back(actual); // Se introduce en el arbol definitivo el nodo
-		if (actual.n_hijos < max_hijos) hijos_libres.push(tree.size()-1); // Se marca el nodo por si necesitamos incluir mas nodos posteriormente
-	}
-	/*for (int i =0; i<tree.size();i++) {
-
-		// PROBAR SI ESTÁN BIEN HECHAS LAS RELACIONES PADRE-HIJO
-		if (tree[i].n_hijos == 0) {
-			std::cout << tree[i].id_nodo << "			" << tree[i].n_hijos << "			" << tree[i].relacion << std::endl;
 		}
 		else {
-			for (int j=0;j<tree[i].n_hijos;j++) {
-				std::cout << "  " << tree[i].id_nodo << "		"<< tree[i].n_hijos << "		" << tree[i].relacion << "			" << tree[i].hijos[j] << std::endl;
-			}
+			actual->n_hijos = 0;
+
+		}
+		arbol.push_back(actual);
+	}
+	/*for (Nodo* i : arbol) {
+		if (i->padre == NULL) std::cout << i->id_nodo << " " << i->n_hijos << " " << "0" << " " << i->relacion << " " << std::endl;
+		else std::cout << i->id_nodo << " " << i->n_hijos << " " << i->padre->id_nodo << " " << i->relacion << " " << std::endl;
+	} */
+	reorderTreeBFS(arbol);
+	std::sort(arbol.begin(), arbol.end(), idSorter);
+	treeData(arbol);
+	graphGenerator(arbol,false);
+	bfstreeToHLS(arbol, bits_needed(nodos));
+	reorderTreeDFS(arbol);
+	//std::sort(arbol.begin(), arbol.end(), idSorter);
+	graphGenerator(arbol, true);
+	dfstreeToHLS(arbol,bits_needed(nodos));
+}
+
+void reorderTreeDFS(std::vector<Nodo*> arbol) {
+	std::stack<Nodo*> pila_dfs;
+	unsigned int counter = 1;
+	Nodo* raiz = arbol[0];
+	Nodo* actual;
+	pila_dfs.push(raiz);
+	while (!pila_dfs.empty()) {
+		actual = pila_dfs.top();
+		pila_dfs.pop();
+		for (auto it = actual->nodos_hijos.rbegin(); it != actual->nodos_hijos.rend(); ++it) pila_dfs.push(*it);
+		actual->id_nodo = counter;
+		counter++;
+		
+	}
+	
+}
+void graphGenerator(std::vector<Nodo*> arbol, bool isDfs) {
+	std::string file_name("graphviz_tree_");
+	if (!isDfs) file_name += "bfs_";
+	else file_name += "dfs_";
+	file_name += std::to_string(tree_size) + ".txt";
+	std::ofstream file(file_name);
+	file << " graph T { " << std::endl;
+	std::string hijos;
+	for (Nodo* i : arbol) { 
+		if (i->n_hijos != 0) {
+			hijos += " {";
+			for (Nodo* j : i->nodos_hijos) hijos += " " + std::to_string(j->id_nodo);
+			//std::cout << hijos << std::endl;
+			hijos += " }";
+			switch (i->relacion) {
+			case 0:
+				file << "   " <<i->id_nodo << "  -- " << hijos << " [color = green];" << std::endl;
+				break;
+			case 1:
+				file << "   " << i->id_nodo << " -- " << hijos << " [color = red];" << std::endl;
+				break;
+			case 2:
+				file << "   " << i->id_nodo << " -- " << hijos << " [color = blue];" << std::endl;
+				break;
+			case 3:
+				file << "   " << i->id_nodo << " -- " << hijos << " [color = yellow];" << std::endl;
+				break;
+
+			} // END SWITCH
+		} // END IF 
+		hijos = "";
+	}// END FOR
+	file << "}" << std::endl;
+	file.close();
+	
+}
+void bfstreeToHLS(std::vector<Nodo*> arbol, unsigned int necessaryBits) {
+	std::string file_name("hls_tree_bfs_" + std::to_string(tree_size) + ".txt");
+	std::ofstream file(file_name);
+	std::queue<Nodo*> cola_bfs;
+	unsigned int palabra;
+	unsigned int padre, hijo;
+	Nodo* i;
+	cola_bfs.push(arbol[0]);
+	while (!cola_bfs.empty()) {
+		i = cola_bfs.front();
+		cola_bfs.pop();
+		padre = i->id_nodo;
+		for (auto it = i->nodos_hijos.begin(); it != i->nodos_hijos.end(); ++it) {
+			Nodo* j = *it;
+			hijo = j->id_nodo;
+			palabra = (((padre & 0xFFFF) << (necessaryBits+2)) | ((hijo & 0xFFFF) << 2) | (i->relacion & 0xFF));
+			file << palabra << std::endl;
+			cola_bfs.push(j);
 		}
 	}
-	*/
-	std::ofstream file ("arbol.txt");
-	for (int i = 0;i<tree.size();i++)  file << tree[i].id_nodo << "			" << tree[i].n_hijos << "			" << tree[i].relacion << "\n";
 	file.close();
 }
+
+void dfstreeToHLS(std::vector<Nodo*> arbol, unsigned int necessaryBits) {
+	std::string file_name("hls_tree_dfs_" + std::to_string(tree_size) + ".txt");
+	std::ofstream file(file_name);
+	std::stack<Nodo*> pila_dfs;
+	unsigned int palabra;
+	unsigned int padre, hijo;
+	Nodo* i = arbol[0];
+	for (auto it = i->nodos_hijos.rbegin(); it != i->nodos_hijos.rend(); ++it) pila_dfs.push(*it);
+
+	while (!pila_dfs.empty()) {
+		i = pila_dfs.top();
+		pila_dfs.pop();
+		padre = i->padre->id_nodo;
+		for (auto it = i->nodos_hijos.rbegin(); it != i->nodos_hijos.rend(); ++it) pila_dfs.push(*it);
+		hijo = i->id_nodo;
+		palabra = (((padre & 0xFFFF) << (necessaryBits + 2)) | ((hijo & 0xFFFF) << 2) | (i->relacion & 0xFF));
+		file << palabra << std::endl;		
+	}
+	file.close();
+}
+
+void treeData(std::vector<Nodo*> arbol) {
+	std::string file_name("arbol_" + std::to_string(tree_size) + ".txt");
+	std::ofstream file(file_name);
+	std::string hijos;
+	file << "ID_NODO" << "    " << "PADRE:   " << "RELACION" << "    " << "ID_HIJOS " << std::endl;
+	for (Nodo* i : arbol) {
+		if (i->id_nodo == 1) {
+			for (Nodo* j : i->nodos_hijos) hijos += " "+std::to_string(j->id_nodo);
+			file << i->id_nodo << "    RAIZ        " << i->relacion << "         " << hijos << std::endl;
+		} else if (i->n_hijos != 0) {
+			for (Nodo* j : i->nodos_hijos) hijos += " "+std::to_string(j->id_nodo);
+			file << i->id_nodo << "          " << i->padre->id_nodo <<"         " << i->relacion << "        " << hijos << std::endl;
+		} else 	file << i->id_nodo << "         " << i->padre->id_nodo << "        " << i->relacion << "      " << "SIN HIJOS" << std::endl;
+		hijos = "";
+	}
+	file.close();
+}
+void reorderTreeBFS(std::vector<Nodo*> arbol) {
+	std::queue<Nodo*> cola_bfs;
+	unsigned int counter = 2;
+	Nodo* raiz = arbol[0];
+	Nodo* actual;
+	cola_bfs.push(raiz);
+	while (!cola_bfs.empty()) {
+		actual = cola_bfs.front();
+		cola_bfs.pop();
+		for (auto it = actual->nodos_hijos.begin(); it != actual->nodos_hijos.end(); ++it) {
+			Nodo* hijo = *it;
+			hijo->id_nodo = counter;
+			cola_bfs.push(hijo);
+			counter++;
+		}
+	}
+	
+}
+
+unsigned int bits_needed(uint32_t value) {
+	int bits = 0;
+	for (int bit_test = 16; bit_test > 0; bit_test >>= 1)
+	{
+		if (value >> bit_test != 0)
+		{
+			bits += bit_test;
+			value >>= bit_test;
+		}
+	}
+	return bits + value;
+}
+int main() {
+	for (int i = 7; i < 16; i++) treeGenerator(pow(2, i), 3);
+
+		return 0;
+	}
+	
