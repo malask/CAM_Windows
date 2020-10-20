06469a0a16a9a4f971c48736006884de066a268d
Manuel Garcia Diaz Santos <manujugon@gmail.com>
2020-10-20 13:00:41 +0200

    Subida de cam binaria con submemorias

--- HLS_cams/cam_binaria_submemorias/cam.cpp
+++ HLS_cams/cam_binaria_submemorias/cam.cpp
@@ -1,89 +1,73 @@
+
 #include "hls_stream.h"
 #include "ap_int.h"
 #include "cam.h"
 
-//#define NODE_MASK 0xF // ((1<<4)-1) Cada nodo ocupa 4 bits. Esto es una máscara que he creado para
-//#define RELATION_MASK 0x3 // ((1<<2)-1 )Expresarlo en hexadecimal.
-//#define TREE_SIZE 1024
 
-// Declaracion de arbol estático
-
-
-void top_function(edge_t tree1[TREE_SIZE/2], edge_t tree2[TREE_SIZE], node_t nodo, rel_t relationship, bool fatherSearch, hls::stream<node_t> &result){
+void top_function(node_t nodo, rel_t relationship, bool fatherSearch, hls::stream<node_t> &result){
 	static hls::stream<node_t> in1("ENTRADA_FOR1");
-	#pragma HLS STREAM variable=in1 depth=8 dim=1
+	#pragma HLS STREAM variable=in1 depth=16 dim=1
 	static hls::stream<node_t> in2("ENTRADA_FOR2");
-	#pragma HLS STREAM variable=in2 depth=8 dim=1
+	#pragma HLS STREAM variable=in2 depth=16 dim=1
 	static hls::stream<node_t> in1b;
-	#pragma HLS STREAM variable=in1b depth=8 dim=1
+	#pragma HLS STREAM variable=in1b depth=16 dim=1
 	static hls::stream<node_t> in2b;
-	#pragma HLS STREAM variable=in2b depth=8 dim=1
+	#pragma HLS STREAM variable=in2b depth=16 dim=1
+	extern edge_t bfstree_2_1[];
+	extern edge_t bfstree_2_2[];
+	extern short size_2_1;
+	extern short size_2_2;
 #pragma HLS DATAFLOW
-	busqueda_cam(tree1, nodo, relationship, fatherSearch, in1,in1b);
-	busqueda_cam(tree2, nodo, relationship, fatherSearch, in2,in2b);
-	combinar(in1,in2,in1b,in2b,result);
+
+	busqueda_cam(bfstree_2_1,size_2_1,nodo, relationship, fatherSearch, in1,in1b);
+	busqueda_cam(bfstree_2_2,size_2_2,nodo, relationship, fatherSearch, in2,in2b);
+	combinar(in1, in2, in1b,in2b,result);
 }
-void busqueda_cam (edge_t tree [TREE_SIZE/2], node_t nodo, rel_t relationship,
-		bool fatherSearch, hls::stream<node_t> &result1,hls::stream<node_t> &result2) {
-#ifndef __SYNTHESIS__
-	int accesos = 0;
-#endif
+void busqueda_cam (edge_t subtree[],short size,node_t nodo, rel_t relationship, bool fatherSearch, hls::stream<node_t> &result1, hls::stream<node_t> &result2) {
 	ap_uint<EDGE_BITS> valor_min, valor_max = 0;
 	ap_uint<REL_BITS> node_relation_min, node_relation_max =0;
-	ap_uint<NODE_BITS> compare_node_min, compare_node_max, founded_node=0;
-	bool flag_min, flag_max= false;
+	ap_uint<NODE_BITS> compare_node_min, compare_node_max;
 	if (nodo == 1 && fatherSearch) {
-		result1.write(EOT);
-		result2.write(EOT);
-		return;
+			result1.write(EOT);
+			result2.write(EOT);
+			return;
 	}
-if (fatherSearch) {
-	for (int i = 0;i<TREE_SIZE/2; i+=2) {
-#pragma HLS PIPELINE
-			  valor_min = tree[i];
-			  node_relation_min = valor_min(1,0);
-			  compare_node_min = DST_NODE(valor_min);
+			if (fatherSearch) {
+				for (int i = 0;i<size; i+=2) {
+			#pragma HLS PIPELINE
+						  valor_min = subtree[i];
+						  node_relation_min = valor_min(1,0);
+						  compare_node_min = DST_NODE(valor_min);
+						  if ((compare_node_min == nodo) && (relationship == node_relation_min))  result1.write(SRC_NODE(valor_min));
+						  valor_max = subtree[i+1];
+						  node_relation_max = valor_max(1,0);
+						  compare_node_max = DST_NODE(valor_max);
+						  if ((compare_node_max == nodo) && (relationship == node_relation_max)) result2.write(SRC_NODE(valor_max));
 
-			  valor_max = tree[i+1];
-			  node_relation_max = valor_max(1,0);
-			  compare_node_max = DST_NODE(valor_max);
+			}
+			} else {
+				for (int i = 0;i<size; i+=2) {
+			#pragma HLS PIPELINE
+						  valor_min = subtree[i];
+						  node_relation_min = valor_min(1,0);
+						  compare_node_min = SRC_NODE(valor_min);
 
-
-// Este if es para buscar al padre del nodo proporcionado a la CAM
-				  if ((compare_node_min == nodo) && (relationship == node_relation_min)) {
-					  result1.write(SRC_NODE(valor_min));
-				  }
-				  if ((compare_node_max == nodo) && (relationship == node_relation_max)) {
-					  result2.write(SRC_NODE(valor_max));
-				  }
-// Este if es para buscar los hijos del nodo proporcionado
-}
-} else {
-	for (int i = 0;i<TREE_SIZE/2; i+=2) {
-#pragma HLS PIPELINE
-			  valor_min = tree[i];
-			  node_relation_min = valor_min(1,0);
-			  compare_node_min = SRC_NODE(valor_min);
-
-			  valor_max = tree[i+1];
-			  node_relation_max = valor_max(1,0);
-			  compare_node_max = SRC_NODE(valor_max);
+						  valor_max = subtree[i+1];
+						  node_relation_max = valor_max(1,0);
+						  compare_node_max = SRC_NODE(valor_max);
 
 
-// Este if es para buscar al padre del nodo proporcionado a la CAM
-				  if ((compare_node_min == nodo) && (relationship == node_relation_min)) {
-					  result1.write(DST_NODE(valor_min));
-				  }
-				  if ((compare_node_max == nodo) && (relationship == node_relation_max)) {
-					  result2.write(DST_NODE(valor_max));
-				  }
-// Este if es para buscar los hijos del nodo proporcionado
-}
-}
-	result1.write(EOT);
-	result2.write(EOT);
-	return;
-}
+			// Este if es para buscar al padre del nodo proporcionado a la CAM
+							  if ((compare_node_min == nodo) && (relationship == node_relation_min)) result1.write(DST_NODE(valor_min));
+
+							  if ((compare_node_max == nodo) && (relationship == node_relation_max)) result2.write(DST_NODE(valor_max));
+			// Este if es para buscar los hijos del nodo proporcionado
+					}
+			}
+				result1.write(EOT);
+				result2.write(EOT);
+				return;
+		}
 
 void combinar(hls::stream<node_t> &in1, hls::stream<node_t> &in2, hls::stream<node_t> &in3, hls::stream<node_t> &in4, hls::stream<node_t> &result) {
 	bool end_1 = false, end_2 = false, end_3 = false, end_4 = false;
@@ -113,6 +97,6 @@ void combinar(hls::stream<node_t> &in1, hls::stream<node_t> &in2, hls::stream<no
 				else result.write(val4);
 			}
 		} while ((!end_1) || (!end_2) || (!end_3) || (!end_4));
-
-		result.write(EOT);
+	result.write(EOT);
 }
+
--- HLS_cams/cam_binaria_submemorias/test_bench.cpp
+++ HLS_cams/cam_binaria_submemorias/test_bench.cpp
@@ -3,111 +3,89 @@
 #include <hls_stream.h>
 #include <iostream>
 #include <fstream>
+//#include "data_128.c"
 
 using namespace std;
-
-
-
-
-int printResult(hls::stream<node_t> &salida_hw, bool fatherOrChild, unsigned int prueba){
-	node_t salida_lectura;
-	if (salida_hw.empty()) {
-			std::cout<< " ---- No nodes detected for this case. Try again ---- " << prueba << " ---- " <<std::endl;
-			return -1;
-		} else {
-			if (fatherOrChild){ std::cout << "Node " << prueba << " 's father is:  " << std::endl;
-			}else std::cout << "Node " << prueba << " 's children is/are: " << std::endl;
-				while(!salida_hw.empty()){
-					salida_lectura = salida_hw.read();
-					if (salida_lectura == EOT) {
-						std::cout << "SEARCH ENDED" << std::endl;
-					}else std::cout << salida_lectura << std::endl;
-				}
-				std::cout << "---- Test succesful at the moment! ----" << std::endl;
-		}
-	return 0;
-}
 
 int main (int argc, char *argv[]) {
 	ifstream infile;
 	string line;
 	unsigned int cnt;
-	edge_t tree[TREE_SIZE];
-	srand((unsigned)time(0));
+	extern int results_parents[];
+	extern int results_childrens[];
+	extern int selected[];
+	extern edge_t bfstree[];
 
 
-	node_t salida_lectura, prueba = 0;
+	node_t salida_lectura;
+	int prueba = 0;
+	int j = 0;
 	rel_t rel = 0;
-
-
+	bool buscarPadre = true;
 	hls::stream<node_t> salida_hw("CANAL_RESULTADO");
 
-	//Leer el arbol del fichero
-	//infile.open(TREE_FILE,ios::in);
-	infile.open(TREE_FILE, ios::in);
-	if (!infile.is_open()) {
-		cout << "Archivo con los datos del árbol no abierto" << endl;
+
+	for (unsigned int i = 0; i < TREE_SIZE/4; i++,j++) {
+		prueba = selected[i];
+		top_function(prueba,rel,buscarPadre,salida_hw);
+		cout << "El padre del nodo " << prueba << "es " << results_parents[i] << std::endl;
+
+		cout << "Ha salido: ";
+		if (salida_hw.empty()) {
+					std::cout<< " ---- No nodes detected for this case. Try again ---- " << prueba << " ---- " <<std::endl;
+
+				} else {
+						while(!salida_hw.empty()){
+							salida_lectura = salida_hw.read();
+							cout << salida_lectura << " " ;
+
+							if (results_parents[i] == EOT && prueba == 1) {
+								assert(salida_lectura == results_parents[i]);
+							}
+							else  {
+								if (salida_lectura != EOT) {
+									assert(salida_lectura == results_parents[i]);
+								}
+							}
+						}
+						cout << endl;
+				}
 	}
-	cout << "FICHERO DE ENTRADA CON NUMERO DE DATOS " << TREE_SIZE << " BUSQUEDA " << SEARCH_MODE <<  endl;
-	cnt = 0;
-	while (getline(infile,line)) {
-		// cout << cnt << " Arco leído: " << line << endl; Sabemos que funciona correctamente.
-		tree[cnt++] = stoul(line);
+
+	buscarPadre = false;
+	int cont_hij = 0;
+	//prueba = 116 aqui esta el conflictivo ;
+	for (int i = 0; i < TREE_SIZE/4; i++,j++) {
+		prueba = selected[i];
+		top_function(prueba,rel,buscarPadre,salida_hw);
+		cout << "Los hijos del nodo " << prueba << " son: ";
+		if (results_childrens[cont_hij] == EOT) {
+			salida_lectura = salida_hw.read();
+			cout << results_childrens[cont_hij] << " y ha salido " << salida_lectura << std::endl;
+			assert(results_childrens[cont_hij] == salida_lectura);
+			cont_hij +=1;
+			if (salida_hw.empty()) cout << "No hay datos" << std::endl;
+			else cout << "Aun hay datos" << std::endl;
+		}
+		else {
+			cout << "Ha salido " ;
+			while (!salida_hw.empty()) {
+				salida_lectura = salida_hw.read();
+				if (salida_lectura == EOT) cout << "FIN DE LA SALIDA" << std::endl;
+				else{
+				cout << "Salida de la cam: " << salida_lectura << std::endl;
+				cout << "El valor en los resultados es : " << results_childrens[cont_hij] << std::endl;
+				cont_hij+=1;
+				}
+
+			}
+		}
 	}
-	bool buscarPadre = true;
-	std::cout << "---- CASE TESTING FOR PARENT CASES ---- " << std::endl;
-	std::cout << "---- Testing nº of access for worst case (i = N) ---- " << std::endl;
-
-	prueba = TREE_SIZE-1;
-
-	top_function(tree,&tree[TREE_SIZE/2],prueba,rel,buscarPadre,salida_hw);
-	printResult(salida_hw,true,prueba);
-	std::cout << "---- Finalized test for worst case ---- " << std::endl;
-	std::cout <<" " << std::endl;
+	std::cout << "El numero de hijos contado es: " << cont_hij << std::endl;
 
 
-	std::cout << "---- Testing nº of access for best case (i = 1) ---- " << std::endl;
-	prueba = 1;
-	top_function(tree,&tree[TREE_SIZE/2],prueba,rel,buscarPadre,salida_hw);
-	printResult(salida_hw,true,prueba);
-	std::cout << "---- Finalized test for best case ---- " << std::endl;
-	std::cout <<" " << std::endl;
-
-
-	std::cout << "---- Testing nº of access for promedium case (i = N/2)" << std::endl;
-	prueba = TREE_SIZE/2;
-	top_function(tree,&tree[TREE_SIZE/2],prueba,rel,buscarPadre,salida_hw);
-	printResult(salida_hw,true,prueba);
-
-	std::cout << "---- Finalized test for promedium case ---- " << std::endl;
-	std::cout << "---- Finalized test for PARENT cases. Starting CHILDREN cases----" << std::endl;
-
-	std::cout << "---- CASE TESTING FOR CHILDREN CASES ---- " << std::endl;
-		buscarPadre = false;
-		std::cout << "---- Testing nº of access for worst case (i = N). ---- " << std::endl;
-
-		prueba = TREE_SIZE-1;
-
-		top_function(tree,&tree[TREE_SIZE/2],prueba,rel,buscarPadre,salida_hw);
-		printResult(salida_hw,false,prueba);
-		std::cout << "---- Finalized test for worst case ---- " << std::endl;
-		std::cout <<" " << std::endl;
-
-
-		std::cout << "---- Testing nº of access for best case (i = 1) ---- " << std::endl;
-		prueba = 1;
-		top_function(tree,&tree[TREE_SIZE/2],prueba,rel,buscarPadre,salida_hw);
-		printResult(salida_hw,false,prueba);
-		std::cout << "---- Finalized test for best case ---- " << std::endl;
-		std::cout <<" " << std::endl;
-
-
-		std::cout << "---- Testing nº of access for promedium case (i = N/2)" << std::endl;
-		prueba = TREE_SIZE/2;
-		top_function(tree,&tree[TREE_SIZE/2],prueba,rel,buscarPadre,salida_hw);
-		printResult(salida_hw,false,prueba);
-	std::cout <<" " << std::endl;
 
 	return 0;
+
 }
 
